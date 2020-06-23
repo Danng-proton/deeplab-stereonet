66cd9151377ae706a1049124f44b262b7bd09713
yifei <15106139449@163.com>
2020-06-23 17:40:51 +0800

    return score_softmax

--- loss_scoremap.py
+++ loss_scoremap.py
@@ -158,7 +158,8 @@ def loss_calc(out, label, target):
         W = torch.exp(-0.5*res.mul(res)).float()
         # m = nn.LogSoftmax()
         out = torch.where(target_list==0,torch.full_like(out, 1), out)
-        out = torch.log(out)
+        out = torch.log_softmax(out)
+        # out = torch.log(out)
         out = out.mul(W)
         return -torch.mean(out)
 
