25f7abd97f4b223fc449b2f7a540f4ff131f9f64
yifei <15106139449@163.com>
2020-06-23 16:34:59 +0800

    add softmax repeat

--- models/StereoNet_deeplab_multi.py
+++ models/StereoNet_deeplab_multi.py
@@ -364,7 +364,7 @@ class StereoNet(nn.Module):
         score_map_softmax= F.softmax(cost, dim=1)
         print("******************score_softmax:", score_map.shape)
         score_map_softmax=torch.unsqueeze(score_map_softmax,1)
-        score_reshape=torch.repeat(1,refimg_feature.size()[1],1,1,1)
+        score_reshape=score_map_softmax.repeat(1,refimg_feature.size()[1],1,1,1)
         # for channal_id in range(refimg_feature.size()[1]):
         #     score_reshape[:,channal_id,:,:,:]=score_map_softmax[:,0,:,:,:]
         out_corr = torch.mul(score_reshape, cost)
